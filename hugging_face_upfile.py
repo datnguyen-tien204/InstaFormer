60f322d17aa5d870647fe8a373d5051f7392e06d
Dat Nguyen-Tien <nguyendatdtqn@outlook.com>
2024-03-17 01:39:08 +0700

    update_initalize

--- hugging_face_upfile.py
+++ hugging_face_upfile.py
@@ -4,7 +4,7 @@ login("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",add_to_git_credential=True)
 api = HfApi()
 
 api.upload_folder(
-    folder_path=r"E:\NLP\InstaFormer\dataset",
-    repo_id="datnguyentien204/InstaFormerDataset",
+    folder_path=r"E:\NLP\InstaFormer\DGNL",
+    repo_id="datnguyentien204/DanhGiaNangLuc",
     repo_type="dataset",multi_commits=True,multi_commits_verbose=True
 )
